55dc8613bc362b7ee49e71de294a723cc86c077c
Ben Knoll <bknoll66@gmail.com>
2018-04-01 20:39:49 -0500

    Function edits

--- liri.js
+++ liri.js
@@ -1,112 +1,126 @@
+var keys = require("./keys.js");
+var request = require("request");
 require("dotenv").config();
+
+var fs = require("file-system");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 var liriCommand = process.argv[2];
 
-var keys = require("./keys.js");
-var request = require("request");
-var fs = require("fs");
-var Twitter = require("twitter");
-var Spotify = require('node-spotify-api')
-var client = new Twitter(keys.twitterKeys);
-var spotify = new Spotify(keys.spotifyKeys);
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
-//Switches between user selected function
-var choice = function(caseData, functionData) {
-    switch (liriCommand) {
-        case 'my-tweets':
-            loadTweets();
-            break;
-        case 'spotify-this-song':
-            mySpotify(liriCommand);
-            break;
-        case 'movie-this':
-            movieThis(liriCommand);
-            break;
-        case 'do-what-it-says':
-            random();
-            break;
-    };
-}
-
-var runFunc = function(argOne, argTwo) {
-    choice(argOne, argTwo);
-};
-(process.argv[2], process.argv[3]);
-
-//tweet retrieval function
-var loadTweets = function() {
-    var params = {
-        screen_name: "Grassy_Knoll88"
-    };
-    client.get("statuses/user_timeline", params, function(
-        error,
-        tweets,
-        response
-    ) {
-        if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
-                console.log('********************');
-                console.log(tweets[i].created_at);
-                console.log(tweets[i].text);
-                console.log('********************');
-            }
-        } else {
-            console.log(error);
-        }
-    });
+console.log(`this is ${client}`);
+var getTweets = function() {
+  var params = { screen_name: "Grassy_Knoll88" };
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (var i = 0; i < 20; i++) {
+        console.log(tweets[i].created_at);
+        console.log(tweets[i].text);
+        console.log("**********");
+      }
+    } else {
+      console.log(error);
+    }
+  });
 };
 
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
 
-//spotify data retrieval function
-function mySpotify(liriCommand) {
-    if (liriCommand == null) {
-        liriCommand = 'The Sign';
+spotify.getSong = function() {
+  console.log("got into");
+  spotify.search({ type: "track", limit: 1, query: process.argv[3] }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
     }
-    spotify.search({
-        type: 'track',
-        query: liriCommand,
-        limit: 5
-    }, function(error, data) {
-        if (error) {
-            console.log('Error occurred: ' + error);
-            return;
-        }
-        console.log('********************');
-        console.log('Artist(s): ' + data.tracks.items[0].artists[0].name);
-        console.log('Song Title: ' + data.tracks.items[0].name);
-        console.log('Preview Link: ' + data.tracks.items[0].preview_url);
-        console.log('Album: ' + data.tracks.items[0].album.name);
-        console.log('********************');
-    });
-}
+    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+    console.log(
+      "Song preview: " +
+        data.tracks.items[0].album.artists[0].external_urls.spotify
+    );
+    console.log("Album name: " + data.tracks.items[0].album.name);
+    console.log("Song title: " + data.tracks.items[0].name);
+  });
+};
 
+var pick = function(caseData, functionData) {
+  switch (liriCommand) {
+    case "my-tweets":
+      getTweets();
+      console.log("here are your tweets");
+      break;
+    case "spotify-this-song":
+      spotify.getSong();
+      console.log("Play this song");
+      break;
+    case "movie-this":
+      noMovie();
+      break;
+    case "do-what-it-says":
+      noCommand();
+      break;
+    default:
+      console.log("This command is foreign to Liri.");
+  }
+};
 
-//OMDB data retrieval function 
-function movieThis(liriCommand) {
-    if (liriCommand == null) {
-        liriCommand = 'Mr. Nobody';
+var movieThis = function() {
+  request(
+    "http://www.omdbapi.com/?apikey=trilogy&t=" + process.argv[3],
+    function(error, response, body) {
+      console.log("**********");
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Year: " + JSON.parse(body).Year);
+      console.log("Rating: " + JSON.parse(body).Rated);
+      console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[0].Value);
+      console.log("Country Produced: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("**********");
+
     }
-    request("http://www.omdbapi.com/?t=" + liriCommand + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log('********************');
-            console.log('Movie Title: ' + JSON.parse(body).Title);
-            console.log('Release Year: ' + JSON.parse(body).Year);
-            console.log('IMDb Rating: ' + JSON.parse(body).imdbRating);
-            console.log('Country: ' + JSON.parse(body).Country);
-            console.log('Language: ' + JSON.parse(body).Language);
-            console.log('Plot: ' + JSON.parse(body).Plot);
-            console.log('Lead Actors: ' + JSON.parse(body).Actors);
-            console.log('********************');
-        }
-    });
-}
+  );
+};
 
-function random() {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-        if (error) {
-            console.log(error);
-        } else {
-            mySpotify(data[1]);
-        }
-    });
-}
+var noMovie = function() {
+  if (process.argv[3] !== "") {
+    process.argv[3] = "Mr. Nobody";
+    movieThis();
+  } else {
+    movieThis();
+  }
+};
+
+var noCommand = function() {
+  fs.readFile("random.txt", "UTF8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    process.argv[3] = data;
+    console.log(data);
+    console.log("Process: " + process.argv[3]);
+    spotify.getSong();
+  });
+};
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+runThis(process.argv[2], process.argv[3]);
